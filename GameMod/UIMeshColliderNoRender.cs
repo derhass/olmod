83ce2931c5b46fa75a2276dc7302825c30754107
derhass <derhass@arcor.de>
2021-03-29 01:51:34 +0200

    try to not render the collider mesh

---
+++ GameMod/UIMeshColliderNoRender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Harmony;
+using Overload;
+using UnityEngine;
+
+namespace GameMod {
+
+    [HarmonyPatch(typeof(UIManager), "GenerateUICollisionMesh")]
+    class UIMeshColliderNoRender_GenerateUICollisionMesh {
+        private static void Postfix() {
+            Overload.UIManager.url[4].c_mesh.Clear();
+        }
+    }
+}
