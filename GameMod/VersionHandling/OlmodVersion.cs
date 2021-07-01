f906e8033e5451be9e6c211a013be620ece296fe
derhass <derhass@arcor.de>
2021-07-01 21:05:26 +0200

    update version comment

--- GameMod/VersionHandling/OlmodVersion.cs
+++ GameMod/VersionHandling/OlmodVersion.cs
@@ -31,7 +31,7 @@ namespace GameMod.VersionHandling
 
                     // do not include revision unless explicitly set to non-zero in the assembly version
                     string maybeRevision = RunningVersion.Revision > 0 ? $".{RunningVersion.Revision}" : "";
-                    _fullVersionString = $"olmod {RunningVersion.ToString(3)}{maybeRevision}{(Modded ? " **MODDED**" : "")} PSDMP";
+                    _fullVersionString = $"olmod {RunningVersion.ToString(3)}{maybeRevision}{(Modded ? " **MODDED**" : "")} PERFDMP";
                 }
                 return _fullVersionString;
             }
